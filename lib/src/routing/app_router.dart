a96d95676af9e90480f5bb23f14f9249d5acfcf4
Mostafa Mohammed Radwan <mostafaradwan1001@gmail.com>
2025-03-03 01:37:13 +0300

    products page navigation

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
 import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
@@ -13,7 +14,8 @@ enum AppRoute {
   cart('cart'),
   orders('orders'),
   account('account'),
-  signIn('signIn');
+  signIn('signIn'),
+  product('product/:id');
 
   const AppRoute(this.path);
   final String path;
@@ -24,49 +26,65 @@ final goRouter = GoRouter(
   initialLocation: AppRoute.home.path,
   routes: [
     GoRoute(
-        path: AppRoute.home.path,
-        name: AppRoute.home.name,
-        builder: (context, state) => ProductsListScreen(),
-        routes: [
-          GoRoute(
-            path: AppRoute.cart.path,
-            name: AppRoute.cart.name,
-            pageBuilder: (context, state) => MaterialPage(
+      path: AppRoute.home.path,
+      name: AppRoute.home.name,
+      builder: (context, state) => ProductsListScreen(),
+      routes: [
+        GoRoute(
+          path: AppRoute.product.path,
+          name: AppRoute.product.name,
+          pageBuilder: (context, state) {
+            final productID = state.pathParameters["id"];
+            return MaterialPage(
               fullscreenDialog: true,
               key: state.pageKey,
-              child: ShoppingCartScreen(),
-            ),
-          ),
-          GoRoute(
-            path: AppRoute.orders.path,
-            name: AppRoute.orders.name,
-            pageBuilder: (context, state) => MaterialPage(
-              fullscreenDialog: true,
-              key: state.pageKey,
-              child: OrdersListScreen(),
-            ),
-          ),
-          GoRoute(
-            path: AppRoute.account.path,
-            name: AppRoute.account.name,
-            pageBuilder: (context, state) => MaterialPage(
-              fullscreenDialog: true,
-              key: state.pageKey,
-              child: AccountScreen(),
-            ),
-          ),
-          GoRoute(
-            path: AppRoute.signIn.path,
-            name: AppRoute.signIn.name,
-            pageBuilder: (context, state) => MaterialPage(
-              fullscreenDialog: true,
-              key: state.pageKey,
-              child: EmailPasswordSignInScreen(
-                formType: EmailPasswordSignInFormType.signIn,
+              child: ProductScreen(
+                productId: productID!,
               ),
+            );
+          },
+        ),
+        GoRoute(
+          path: AppRoute.cart.path,
+          name: AppRoute.cart.name,
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            key: state.pageKey,
+            child: ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: AppRoute.orders.path,
+          name: AppRoute.orders.name,
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            key: state.pageKey,
+            child: OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: AppRoute.account.path,
+          name: AppRoute.account.name,
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            key: state.pageKey,
+            child: AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: AppRoute.signIn.path,
+          name: AppRoute.signIn.name,
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            key: state.pageKey,
+            child: EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
             ),
-          )
-        ]),
+          ),
+        )
+      ],
+      
+    ),
   ],
 );
 
