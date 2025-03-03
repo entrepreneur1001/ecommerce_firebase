bfe23e739a6689f37fd62ccce0afb0a8385625bf
Mostafa Mohammed Radwan <mostafaradwan1001@gmail.com>
2025-03-04 00:42:37 +0300

    use riverpod to read from data source

--- lib/src/features/products/presentation/product_screen/product_screen.dart
+++ lib/src/features/products/presentation/product_screen/product_screen.dart
@@ -1,5 +1,5 @@
-import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/add_to_cart/add_to_cart_widget.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/home_app_bar.dart';
 import 'package:ecommerce_app/src/features/products/presentation/product_screen/leave_review_action.dart';
 import 'package:ecommerce_app/src/features/products/presentation/product_screen/product_average_rating.dart';
@@ -13,6 +13,7 @@ import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_two_column_layout.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Shows the product page for a given product ID.
 class ProductScreen extends StatelessWidget {
@@ -21,26 +22,28 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Read from data source
-    final product =
-        kTestProducts.firstWhere((product) => product.id == productId);
     return Scaffold(
-      appBar: const HomeAppBar(),
-      // ignore: unnecessary_null_comparison
-      body: product == null
-          ? EmptyPlaceholderWidget(
-              message: 'Product not found'.hardcoded,
-            )
-          : CustomScrollView(
-              slivers: [
-                ResponsiveSliverCenter(
-                  padding: const EdgeInsets.all(Sizes.p16),
-                  child: ProductDetails(product: product),
-                ),
-                ProductReviewsList(productId: productId),
-              ],
-            ),
-    );
+        appBar: const HomeAppBar(),
+        // ignore: unnecessary_null_comparison
+        body: Consumer(
+          builder: (context, ref, child) {
+            final productRepository = ref.watch(productsRepositoryProvider);
+            final product = productRepository.getProduct(productId);
+            return product == null
+                ? EmptyPlaceholderWidget(
+                    message: 'Product not found'.hardcoded,
+                  )
+                : CustomScrollView(
+                    slivers: [
+                      ResponsiveSliverCenter(
+                        padding: const EdgeInsets.all(Sizes.p16),
+                        child: ProductDetails(product: product),
+                      ),
+                      ProductReviewsList(productId: productId),
+                    ],
+                  );
+          },
+        ));
   }
 }
 
